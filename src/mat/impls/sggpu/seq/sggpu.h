707bd9f91627f829d468a4ffb2901af20a96c730
Justin Holewinski <justin.holewinski@gmail.com>
2012-06-15 10:42:31 -0400

    Add misc. cleanup for SGGPU
    Hg-commit: af227c54a047c91f4ccde4a7e620271fb60f49fe

--- src/mat/impls/sggpu/seq/sggpu.h
+++ src/mat/impls/sggpu/seq/sggpu.h
@@ -4,8 +4,22 @@
 
 #include "petsc-private/matimpl.h"
 
+// External decls
 EXTERN_C_BEGIN
 extern PetscErrorCode MatCreate_SeqSGGPU(Mat);
 EXTERN_C_END
+
+
+// Matrix type container
+typedef struct {
+  PetscScalar * hostData;   //< Host data
+  PetscInt      stpoints;   //< Number of stencil points
+  PetscInt      dof;        //< Degrees of freedom
+  PetscInt      m;          //< Grid size (x)
+  PetscInt      n;          //< Grid size (y)
+  PetscInt      p;          //< Grid size (z)
+
+  PetscScalar * deviceData; //< Device data
+} Mat_SeqSGGPU;
 
 #endif
