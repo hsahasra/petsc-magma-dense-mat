eafe6aee1d0555796072fc8e4af118a327561233
Chekuri S. Choudary <cchoudary@rnet-tech.com>
2013-02-26 18:00:28 -0500

    Initial working version of MPISGGPU type. 'mpirun -np 2 ./ex14 -dm_mat_type mpisggpu -dm_vec_type mpicusp' gives the same result as single processor. Needs to be further tested for scalability and correctness. Also, different number of processes need to be handled.
    Hg-commit: a5f86cf9e5063817a4842481972e0cf69844b026

--- src/mat/impls/sggpu/mpi/mmsggpu.cu
+++ src/mat/impls/sggpu/mpi/mmsggpu.cu
@@ -2,13 +2,13 @@
 #include <petscblaslapack.h>
 
 
-//This function is a copy of MatSetUpMultiply_Dense(). Need to edit this function to make it work for MPISGGPU. - Chekuri
-
 #undef __FUNCT__
 #define __FUNCT__ "MatSetUpMultiply_MPISGGPU"
-PetscErrorCode MatSetUpMultiply_MPISGGPU(Mat mat)
+PetscErrorCode MatSetUpMultiply_MPISGGPU(Mat A)
 {
-  Mat_MPISGGPU *sggpu = (Mat_MPISGGPU*)mat->data;
+  Mat_MPISGGPU *mat = (Mat_MPISGGPU*)A->data;
+  Mat_SeqSGGPU *mat_seq = mat->mat_seq;
+	
   PetscErrorCode ierr;
   IS           from,to;
   Vec          gvec;
@@ -18,28 +18,28 @@ PetscErrorCode MatSetUpMultiply_MPISGGPU(Mat mat)
   
   MPI_Comm_size(PETSC_COMM_WORLD,&numprocs);
   /* Create local vector that is used to scatter into */
-//  ierr = VecCreateSeq(PETSC_COMM_SELF,mat->cmap->N,&mat->lvec);CHKERRQ(ierr);
+  ierr = VecCreateSeq(PETSC_COMM_SELF,A->cmap->N,&mat->lvec);CHKERRQ(ierr);
 
   /* Create temporary index set for building scatter gather */
-//  ierr = ISCreateStride(((PetscObject)mat)->comm,mat->cmap->N,0,1,&from);CHKERRQ(ierr);
-//  ierr = ISCreateStride(PETSC_COMM_SELF,mat->cmap->N,0,1,&to);CHKERRQ(ierr);
+  ierr = ISCreateStride(((PetscObject)A)->comm,A->cmap->N,0,1,&from);CHKERRQ(ierr);
+  ierr = ISCreateStride(PETSC_COMM_SELF,A->cmap->N,0,1,&to);CHKERRQ(ierr);
 
   /* Create temporary global vector to generate scatter context */
   /* n    = mdn->cowners[mdn->rank+1] - mdn->cowners[mdn->rank]; */
 
-//  ierr = VecCreateMPIWithArray(((PetscObject)mat)->comm,1,((mat->cmap->N)/numprocs),mat->cmap->N,PETSC_NULL,&gvec);CHKERRQ(ierr);
+   ierr = VecCreateMPIWithArray(((PetscObject)A)->comm,1,((A->cmap->N)/mat->size),A->cmap->N,PETSC_NULL,&gvec);CHKERRQ(ierr);
 
   /* Generate the scatter context */
-//  ierr = VecScatterCreate(gvec,from,sggpu->lvec,to,&sggpu->Mvctx);CHKERRQ(ierr);
-//  ierr = PetscLogObjectParent(mat,sggpu->Mvctx);CHKERRQ(ierr);
-//  ierr = PetscLogObjectParent(mat,sggpu->lvec);CHKERRQ(ierr);
-//  ierr = PetscLogObjectParent(mat,from);CHKERRQ(ierr);
-//  ierr = PetscLogObjectParent(mat,to);CHKERRQ(ierr);
-//  ierr = PetscLogObjectParent(mat,gvec);CHKERRQ(ierr);
+  ierr = VecScatterCreate(gvec,from,mat->lvec,to,&mat->Mvctx);CHKERRQ(ierr);
+  ierr = PetscLogObjectParent(A,mat->Mvctx);CHKERRQ(ierr);
+  ierr = PetscLogObjectParent(A,mat->lvec);CHKERRQ(ierr);
+  ierr = PetscLogObjectParent(A,from);CHKERRQ(ierr);
+  ierr = PetscLogObjectParent(A,to);CHKERRQ(ierr);
+  ierr = PetscLogObjectParent(A,gvec);CHKERRQ(ierr);
 
-//  ierr = ISDestroy(&to);CHKERRQ(ierr);
-//  ierr = ISDestroy(&from);CHKERRQ(ierr);
-//  ierr = VecDestroy(&gvec);CHKERRQ(ierr);
+  ierr = ISDestroy(&to);CHKERRQ(ierr);
+  ierr = ISDestroy(&from);CHKERRQ(ierr);
+  ierr = VecDestroy(&gvec);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 
 }
